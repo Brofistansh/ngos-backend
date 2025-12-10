135970a06117cfac46dd218479364763cd28895c
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-12-11 01:15:05 +0530

    Fix NGO controller mapping + middleware functions

--- src/routes/ngo.js
+++ src/routes/ngo.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  createNGO,
-  getNGOs,
+  createNgo,
+  getNgos,
   updateNgo,
   deleteNgo
 } = require('../controllers/ngoController');
@@ -12,10 +12,10 @@ const auth = require('../middlewares/authMiddleware');
 const roles = require('../middlewares/roleMiddleware');
 
 // Only super admin can create NGO
-router.post('/', auth, roles("super_admin"), createNGO);
+router.post('/', auth, roles("super_admin"), createNgo);
 
 // Any authenticated user can view NGOs
-router.get('/', auth, getNGOs);
+router.get('/', auth, getNgos);
 
 // Update NGO
 router.put('/:id', auth, roles("super_admin"), updateNgo);
