09e0a5acd1fd8ea2f9b56ecb3a342dfd51f3a326
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-12-11 04:45:17 +0530

    Fix roleMiddleware returning wrong value

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -1,11 +1,12 @@
-module.exports = function requireRole(...allowedRoles) {
+module.exports = function roles(...allowedRoles) {
   return (req, res, next) => {
+
     if (!req.user) {
-      return res.status(401).json({ message: "Not authenticated" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Forbidden: Access denied" });
+      return res.status(403).json({ message: "Forbidden: Insufficient permissions" });
     }
 
     next();
