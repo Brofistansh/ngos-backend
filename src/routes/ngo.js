2712c1d743f1a39c17d65fdf360c45cdb9af4bb9
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-12-11 01:34:10 +0530

    Full rewrite ngo.js

--- src/routes/ngo.js
+++ src/routes/ngo.js
@@ -1,26 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-  createNgo,
-  getNGOs,
-  updateNgo,
-  deleteNgo
-} = require('../controllers/ngoController');
-
+const ngoController = require('../controllers/ngoController');
 const auth = require('../middlewares/authMiddleware');
 const roles = require('../middlewares/roleMiddleware');
 
+// DEBUG
+console.log("NGO ROUTE LOADED", {
+  createNgo: typeof ngoController.createNgo,
+  getNGOs: typeof ngoController.getNGOs,
+  updateNgo: typeof ngoController.updateNgo,
+  deleteNgo: typeof ngoController.deleteNgo,
+  auth: typeof auth,
+  rolesResult: typeof roles
+});
+
 // Only super admin can create NGO
-router.post('/', auth, roles("super_admin"), createNgo);
+router.post(
+  '/',
+  auth,
+  roles("super_admin"),
+  ngoController.createNgo
+);
 
 // Any authenticated user can view NGOs
-router.get('/', auth, getNGOs);
+router.get(
+  '/',
+  auth,
+  ngoController.getNGOs
+);
 
 // Update NGO
-router.put('/:id', auth, roles("super_admin"), updateNgo);
+router.put(
+  '/:id',
+  auth,
+  roles("super_admin"),
+  ngoController.updateNgo
+);
 
 // Soft delete NGO
-router.delete('/:id', auth, roles("super_admin"), deleteNgo);
+router.delete(
+  '/:id',
+  auth,
+  roles("super_admin"),
+  ngoController.deleteNgo
+);
 
 module.exports = router;
