3b9ea0055e89bf8bdf8408b352557fc13a42d7ff
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-12-11 01:28:06 +0530

    Fix NGO controller function names

--- src/routes/ngo.js
+++ src/routes/ngo.js
@@ -2,25 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  createNGO,
+  createNgo,
   getNGOs,
-  updateNGO,
-  deleteNGO
+  updateNgo,
+  deleteNgo
 } = require('../controllers/ngoController');
 
 const auth = require('../middlewares/authMiddleware');
 const roles = require('../middlewares/roleMiddleware');
 
 // Only super admin can create NGO
-router.post('/', auth, roles("super_admin"), createNGO);
+router.post('/', auth, roles("super_admin"), createNgo);
 
 // Any authenticated user can view NGOs
 router.get('/', auth, getNGOs);
 
 // Update NGO
-router.put('/:id', auth, roles("super_admin"), updateNGO);
+router.put('/:id', auth, roles("super_admin"), updateNgo);
 
 // Soft delete NGO
-router.delete('/:id', auth, roles("super_admin"), deleteNGO);
+router.delete('/:id', auth, roles("super_admin"), deleteNgo);
 
 module.exports = router;
