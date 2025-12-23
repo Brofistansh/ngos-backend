3baa2d3e49845717fc7712fc068e16df8f9e8b4b
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-12-23 23:21:54 +0530

    Fix duplicate User import & add ManagerCenter associations

--- src/models/sequelize/index.js
+++ src/models/sequelize/index.js
@@ -1,49 +1,76 @@
+const sequelize = require("../../db/postgres");
+
+/**
+ * MODELS
+ */
 const User = require("./User");
-const Teacher = require("./Teacher");
-const TeacherTimesheet = require("./TeacherTimesheet");
-
-// --------------------
-// ASSOCIATIONS
-// --------------------
-
-// Teacher ↔ User
-Teacher.belongsTo(User, {
-  foreignKey: "user_id",
-});
-User.hasOne(Teacher, {
-  foreignKey: "user_id",
-});
-
-// Teacher ↔ TeacherTimesheet
-Teacher.hasMany(TeacherTimesheet, {
-  foreignKey: "teacher_id",
-});
-TeacherTimesheet.belongsTo(Teacher, {
-  foreignKey: "teacher_id",
-});
-
-module.exports = {
-  User,
-  Teacher,
-  TeacherTimesheet,
-};
-const ManagerCenter = require("./ManagerCenter");
+const NGO = require("./NGO");
 const Center = require("./Center");
-const User = require("./User");
+const Teacher = require("./Teacher");
+const Student = require("./Student");
+const ManagerDetails = require("./ManagerDetails");
+const ManagerCenter = require("./ManagerCenter");
 
-// Manager (User) ↔ Centers
+/**
+ * ============================
+ * EXISTING ASSOCIATIONS
+ * ============================
+ */
+
+// NGO → Centers
+NGO.hasMany(Center, { foreignKey: "ngo_id" });
+Center.belongsTo(NGO, { foreignKey: "ngo_id" });
+
+// NGO → Users
+NGO.hasMany(User, { foreignKey: "ngo_id" });
+User.belongsTo(NGO, { foreignKey: "ngo_id" });
+
+// Center → Users
+Center.hasMany(User, { foreignKey: "center_id" });
+User.belongsTo(Center, { foreignKey: "center_id" });
+
+// User → ManagerDetails
+User.hasOne(ManagerDetails, {
+  foreignKey: "user_id",
+  as: "manager_details",
+});
+ManagerDetails.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+/**
+ * ============================
+ * ✅ NEW: MANAGER ↔ CENTER (MANY TO MANY)
+ * ============================
+ */
+
+// Manager (User) → ManagerCenter
 User.hasMany(ManagerCenter, {
   foreignKey: "manager_id",
 });
 
+// ManagerCenter → User
 ManagerCenter.belongsTo(User, {
   foreignKey: "manager_id",
 });
 
+// Center → ManagerCenter
 Center.hasMany(ManagerCenter, {
   foreignKey: "center_id",
 });
 
+// ManagerCenter → Center
 ManagerCenter.belongsTo(Center, {
   foreignKey: "center_id",
 });
+
+module.exports = {
+  sequelize,
+  User,
+  NGO,
+  Center,
+  Teacher,
+  Student,
+  ManagerDetails,
+  ManagerCenter,
+};
