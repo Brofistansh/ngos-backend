17f6ad558316132f785f3e6f1f9cd23ffa22df10
Ansh Jaiswal <jaiansh2016@gmail.com>
2026-01-03 16:52:33 +0530

    updated postgres.js

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -1,22 +1,52 @@
-const { Sequelize } = require('sequelize');
-const config = require('../../config');
+const { Sequelize } = require("sequelize");
+const config = require("../../config");
 
-const sequelize = new Sequelize(
-  config.DB_NAME,
-  config.DB_USER,
-  config.DB_PASS,
-  {
-    host: config.DB_HOST,
-    port: config.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
-  }
-);
+    })
+  : new Sequelize(
+      config.DB_NAME,
+      config.DB_USER,
+      config.DB_PASS,
+      {
+        host: config.DB_HOST,
+        port: config.DB_PORT,
+        dialect: "postgres",
+        logging: false,
+      }
+    );
 
 module.exports = sequelize;
+
+
+
+// const { Sequelize } = require('sequelize');
+// const config = require('../../config');
+
+// const sequelize = new Sequelize(
+//   config.DB_NAME,
+//   config.DB_USER,
+//   config.DB_PASS,
+//   {
+//     host: config.DB_HOST,
+//     port: config.DB_PORT,
+//     dialect: 'postgres',
+//     logging: false,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   }
+// );
+
+// module.exports = sequelize;
